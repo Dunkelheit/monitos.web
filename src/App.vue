4f1b71fb8826ac19b6f8bc951048ace76f3040ad
Arturo Martinez Minguez <arturo@icemobile.com>
2016-10-31 11:03:17 +0100

    fix: code styling

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,15 @@ export default {
     Hello
   }
 }
-
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
-
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,7 +7,7 @@
             <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
             <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
             <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-            <br>
+            <br />
             <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
         </ul>
         <h2>Ecosystem</h2>
@@ -34,21 +34,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
 a {
-  color: #42b983;
+    color: #42b983;
 }
-
 </style>
