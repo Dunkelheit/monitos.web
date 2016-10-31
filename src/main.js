f0cb0c435bcc55db7b4d8c3228d7304c21d60269
Arturo Martinez Minguez <arturo@icemobile.com>
2016-10-31 17:00:45 +0100

    feat: add vue-resource

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import VueSocketio from 'vue-socket.io';
 import App from './App';
 
 Vue.use(VueSocketio, 'http://localhost:9183');
+Vue.use(VueResource);
 
 new Vue({
     sockets: {
