cb9a9cc14472f13143f1ad0ab884c2e486cb38c9
Arturo Martinez Minguez <arturo@icemobile.com>
2016-10-31 13:48:34 +0100

    feat: integrate cytoscape

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="hello">
+        <img src="../assets/logo.png">
         <h1>{{ msg }}</h1>
         <h2>Essential Links</h2>
         <ul>
@@ -7,7 +8,7 @@
             <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
             <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
             <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-            <br />
+            <br/>
             <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
         </ul>
         <h2>Ecosystem</h2>
@@ -21,33 +22,37 @@
 </template>
 
 <script>
-export default {
-    name: 'hello',
-    data () {
-        return {
-            msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'hello',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            };
         }
     }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-    font-weight: normal;
-}
+    .hello {
+        text-align: center;
+    }
 
-ul {
-    list-style-type: none;
-    padding: 0;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-a {
-    color: #42b983;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
